cba7a5f75c7273f51d06bda6a80dd6a5c566567c
Sordhiq <39282470+Sordhiq@users.noreply.github.com>
2025-07-19 19:08:06 +0100

    Update ssi.py

--- ssi.py
+++ ssi.py
@@ -55,7 +55,7 @@ if page == "Homepage":
     👉 Use the sidebar (>>) at the top-left corner to navigate through the app.
     """)
     st.info("Proudly developed by:")
-    st.markdown("📌 Sodiq Jinad /n 📌 Jimoh Yusrah Tosin")
+    st.markdown("📌 Sodiq Jinad"/n "📌 Jimoh Yusrah Tosin")
     #st.markdown("📌 Jimoh Yusrah Tosin")
 
 # -----------------------------
@@ -85,7 +85,7 @@ elif page == "Dashboard Overview" and df is not None:
     st.bar_chart(avg_sir_op)
 
     st.info("Proudly developed by:")
-    st.markdown("📌 Sodiq Jinad /n 📌 Jimoh Yusrah Tosin")
+    st.markdown("📌 Sodiq Jinad"/n "📌 Jimoh Yusrah Tosin")
     #st.markdown("📌 Jimoh Yusrah Tosin")
 
 # -----------------------------
@@ -115,7 +115,7 @@ elif page == "Hypothesis Testing" and df is not None:
     plt.xticks(rotation=30)
     st.pyplot(fig2)
 
-    st.markdown("📌 Sodiq Jinad /n 📌 Jimoh Yusrah Tosin")
+    st.markdown("📌 Sodiq Jinad"/n "📌 Jimoh Yusrah Tosin")
     #st.markdown("📌 Jimoh Yusrah Tosin")
 # -----------------------------
 # Policy Recommendations
@@ -149,7 +149,7 @@ elif page == "Policy Recommendations" and df is not None:
             st.markdown("### Tailored Recommendations")
             st.write(response.text)
 
-            st.markdown("📌 Sodiq Jinad /n 📌 Jimoh Yusrah Tosin")
+            st.markdown("📌 Sodiq Jinad"/n "📌 Jimoh Yusrah Tosin")
             #st.markdown("📌 Jimoh Yusrah Tosin")
 
         except KeyError:
